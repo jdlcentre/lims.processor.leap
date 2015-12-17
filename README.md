9932781e5a766e97e586b8cacb92fc551d96f152
Adam Brown <adam.brown@evbane.com>
2015-12-17 13:23:29 +0800

    Updated with Pawsey support.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # lims.processor.leap
 Cameca LEAP Processor for LIMS
+
+You must download ashell.py and aterm.jar from https://data.pawsey.org.au/tools/ and store them in this directory.
--- leap_import.py
+++ leap_import.py
@@ -1,13 +1,25 @@
 #!/usr/bin/python
-import os.path
+import os
+from subprocess import call
 import sys
 
 script_path = os.path.dirname(os.path.realpath(__file__))
-zip_path = sys.argv[1]
+rhit_path = sys.argv[1]
 igsn = sys.argv[2]
-ar = sys.argv[3]
+ar = str(sys.argv[3])
 
-project_size = os.path.getsize(zip_path)
+project_size = os.path.getsize(rhit_path)
 
+with open('credentials.config', 'r') as f:
+    credentials = f.readlines()
 
-print 'Path to file, URL etc.' + ';' + str(project_size)
+username = credentials[0]
+password = credentials[1]
+    
+call(['python', 'ashell.py', 'login', username, password], stdout=open(os.devnull, 'wb'))
+
+call(['python', 'ashell.py', 'cf Atom Probe Tomography + mkfolder ' + ar + ' + cf ' + ar + ' + mkfolder ' + igsn + ' + cf ' + igsn + ' + put ' + rhit_path], stdout=open(os.devnull, 'wb'))
+
+call(['python', 'ashell.py', 'logout'], stdout=open(os.devnull, 'wb'))
+
+print 'https://data.pawsey.org.au/public/?path=/Atom Probe Tomography/' + ar + '/' + igsn + '/' + os.path.basename(rhit_path) + ';' + str(project_size)
